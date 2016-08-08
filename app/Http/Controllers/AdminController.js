5e970b84e0d9dfbafcff1842f84fa66a46cf76b4
Joshua Rivers <joshua.rivers@gmail.com>
2016-08-08 08:06:02 -0400

    Have basic setup done for admins

---
+++ app/Http/Controllers/AdminController.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const User = use('App/Model/User')
+const Plant = use('App/Model/Plant')
+
+
+class AdminController {
+
+  * index(request, response) {
+    // console.log(request.authUser.attributes.admin);
+    if (request.authUser.attributes.admin === true) {
+      return response.json({ success: true })
+    } else {
+      return { error: false }
+    }
+  }
+
+  * plantStore(request, response) {
+    const plant = request.all();
+    const admin = request.authUser.attributes.admin;
+    if (admin) {
+      const newPlant = yield Plant.create(plant);
+      return response.json(newPlant.toJSON());
+    } else {
+      return response.json({error: 'Not An Admin'})
+    }
+  }
+
+}
+
+module.exports = AdminController
